1dac04a6dff962b7c27f94f856df2823e6b6afb0
Lee-hyunjun <msh794000@gmail.com>
2024-06-12 21:50:50 +0900

    수정

--- Assets/Script/UI/CharPickUI.cs
+++ Assets/Script/UI/CharPickUI.cs
@@ -8,6 +8,8 @@ using UnityEngine.UIElements;
 
 public class CharPickUI : MonoBehaviour
 {
+
+    public int charNum = 0;
     public Vector3 mainPickPosition = new Vector3 (0, 0, 0);
     //public Vector3 leftPickPosition = new Vector3(-225, 100, 0);
     //public Vector3 rightPickPosition = new Vector3(250, 100, 0);
@@ -31,6 +33,7 @@ public class CharPickUI : MonoBehaviour
 
     public void LeftBtn()
     {
+
         Bubble();
         SetInfo();
         ColorSet();
@@ -41,36 +44,43 @@ public class CharPickUI : MonoBehaviour
         SetInfo();
         ColorSet();
     }
+
     public void Bubble()
     {
-        //좌로 밀기 앞으로 한칸
-        PlayerSlot temp = pickList[0];
-        Vector3 tempRectTransform = temp.rectTransform.anchoredPosition;
-        Vector3 tempScale = temp.rectTransform.localScale;
-        for (int i = 0; i < pickList.Length - 1; i++)
+
+
+        //좌로 밀기
+        Vector3[] localScales = new Vector3[pickList.Length];
+        Vector3[] anchoredPositions = new Vector3[pickList.Length];
+        for (int i = 0; i <= pickList.Length - 1; i++)
         {
-            pickList[i].rectTransform.localScale = pickList[i + 1].rectTransform.localScale;
-            pickList[i].rectTransform.anchoredPosition = pickList[i + 1].rectTransform.anchoredPosition;
-            pickList[i] = pickList[i + 1];
+            localScales[i] = pickList[i].rectTransform.localScale;
+            anchoredPositions[i] = pickList[i].rectTransform.anchoredPosition;
         }
-        pickList[pickList.Length - 1].rectTransform.anchoredPosition = tempRectTransform;
-        pickList[pickList.Length - 1].rectTransform.localScale = tempScale;
-        pickList[pickList.Length - 1] = temp;
+
+            //앞으로한칸
+        PlayerSlot temp = pickList[pickList.Length - 1];
+        Vector3 tempRectTransform = pickList[0].rectTransform.anchoredPosition;
+        Vector3 tempScale = pickList[0].rectTransform.localScale;
+
+        for (int i = pickList.Length - 2; i >= 0; i--)
+        {pickList[i + 1] = pickList[i];}
+
+        pickList[0].rectTransform.anchoredPosition = tempRectTransform;
+        pickList[0].rectTransform.localScale = tempScale;
+        pickList[0] = temp;
+
+        for (int i = 0; i <= pickList.Length-1; i++)
+        {
+            pickList[i].rectTransform.localScale = localScales[i];
+            pickList[i].rectTransform.anchoredPosition = anchoredPositions[i];
+        }
     }
+
     public void ColorSet() 
     {
-        Color color = pickList[0].character.color;
-        color.a = 255f;
-        pickList[0].character.color = color;
 
-        Color color2 = pickList[0].weapon.color;
-        color2.a = 255f;
-        pickList[0].weapon.color = color2;
-
-        //pickList[0].weapon.color = mainColor;
-
-
-        for (int i = 1; i <= pickList.Length-1; ++i) 
+        for (int i = 0; i <= pickList.Length-1; ++i) 
         {
             Debug.Log("들어오긴해?");
             Color color3 = pickList[i].character.color;
@@ -83,6 +93,16 @@ public class CharPickUI : MonoBehaviour
             //pickList[i].character.color = otherColor;
             //pickList[i].weapon.color = otherColor;
         }
+
+
+        Color color = pickList[0].character.color;
+        color.a = 255f;
+        pickList[0].character.color = color;
+
+        Color color2 = pickList[0].weapon.color;
+        color2.a = 255f;
+        pickList[0].weapon.color = color2;
+
     }
     public void SetInfo() 
     {
@@ -91,27 +111,37 @@ public class CharPickUI : MonoBehaviour
             PlayerSO CurSO = pickList[0].playerSO;
         }
     }
-    public void BubbleReverse() 
+    public void BubbleReverse()
     {
-        //우로 밀기
-
-       //뒤로한칸
-        PlayerSlot temp = pickList[pickList.Length - 1];
-        Vector3 tempRectTransform = temp.rectTransform.anchoredPosition;
-        Vector3 tempScale = temp.rectTransform.localScale;
-
-        for (int i = pickList.Length - 2; i >= 0; i--) 
+        Vector3[] localScales = new Vector3[pickList.Length];
+        Vector3[] anchoredPositions = new Vector3[pickList.Length];
+        for (int i = 0; i < pickList.Length; i++)
         {
-            pickList[i + 1].rectTransform.localScale = pickList[i].rectTransform.localScale;
-            pickList[i + 1].rectTransform.anchoredPosition = pickList[i].rectTransform.anchoredPosition;
-            pickList[i + 1] = pickList[i];
+            localScales[i] = pickList[i].rectTransform.localScale;
+            anchoredPositions[i] = pickList[i].rectTransform.anchoredPosition;
         }
-        pickList[0].rectTransform.anchoredPosition = tempRectTransform;
-        pickList[0].rectTransform.localScale = tempScale;
-        pickList[0] = temp;
+        Debug.Log(localScales.Length);
 
+        //우로 밀기 뒤로 한칸
+        PlayerSlot temp = pickList[0];
+        Vector3 tempRectTransform = pickList[pickList.Length - 1].rectTransform.anchoredPosition;
+        Vector3 tempScale = pickList[pickList.Length - 1].rectTransform.localScale;
+        for (int i = 0; i < pickList.Length - 1; i++)
+        {
+            pickList[i] = pickList[i + 1];
+        }
+        pickList[pickList.Length - 1].rectTransform.anchoredPosition = tempRectTransform;
+        pickList[pickList.Length - 1].rectTransform.localScale = tempScale;
+        pickList[pickList.Length - 1] = temp;
+
+        for (int i = 0; i < pickList.Length; i++)
+        {
+            pickList[i].rectTransform.localScale = localScales[i];
+            pickList[i].rectTransform.anchoredPosition = anchoredPositions[i];
+        }
 
     }
+    
     public void OnPickUi() 
     {
         this.gameObject.SetActive(true);
